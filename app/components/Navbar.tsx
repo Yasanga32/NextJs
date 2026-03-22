0a7f11d7cf2adb1618cf5837c06c47ace8316873
Yasanga32 <yasangashanuka33@gmail.com>
2026-03-22 13:10:28 +0530

    added components

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <header>
+        <nav>
+            <Link href="/" className='logo'>
+                <Image src="/icon/logo.png" alt="logo"
+                width={24}
+                height={24}/>
+
+                <p>DevEvent</p>
+                
+            </Link>
+
+                <ul>
+                    <Link href="/">Home</Link>
+                    <Link href="/">Events</Link>
+                    <Link href="/">Create</Link>
+                </ul>
+        </nav>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import ExploreBtn from './components/ExploreBtn'
+import EventCard from './components/EventCard'
+import { time } from 'console'
+import events from '@/lib/constants'
+
 
 function Page() {
   return (
     <section>
         <h1 className='text-center'>The Hub For Every Dev<br/>Event You Cant Miss</h1>
         <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in one place</p>
+        <ExploreBtn/>
+
+        <div className='mt-20 space-y-7'>
+            <h3>Featured Events</h3>
+
+            <ul className='events'>
+              {events.map((event)=>(
+                <li key={event.title}>
+                  <EventCard {...event}/>
+                </li>
+              ))}
+            </ul>
+        </div>
     </section>
   )
 }
